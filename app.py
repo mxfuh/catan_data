8d31528260db9b69103db9d17b3905944fe04b35
Max <max.fuhlendorf@web.de>
2023-12-01 11:59:22 +0100

    first map version

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import numpy as np
 import pandas as pd
-import streamlit
-import plotly
+import streamlit as st
+import plotly.express as px
 
 
 cols = list(pd.read_excel("catan_data.xlsx").iloc[0,:])
@@ -9,3 +9,18 @@ data = pd.read_excel("catan_data.xlsx").iloc[1:,:]
 data.columns = cols
 
 
+# Split 'geoloc' into two columns 'latitude' and 'longitude'
+data[['latitude', 'longitude']] = data['geoloc'].str.split(', ', expand=True)
+data['latitude'] = pd.to_numeric(data['latitude'])
+data['longitude'] = pd.to_numeric(data['longitude'])
+
+# Create a Plotly map
+fig = px.scatter_geo(data,
+                     lat='latitude',
+                     lon='longitude',
+                     hover_name='loc', # this column will be displayed in the hover information
+                     )
+
+# Integrate with Streamlit
+st.title('Interactive Map Dashboard')
+st.plotly_chart(fig)
