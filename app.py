a25fd335dd343be943b398f1ada2ca4b76e043c9
Max <max.fuhlendorf@web.de>
2023-12-01 13:23:05 +0100

    update map details

--- app.py
+++ app.py
@@ -17,11 +17,16 @@ data['longitude'] = pd.to_numeric(data['longitude'])
 
 # Calculate mean scores for each player at each location
 mean_scores = data.groupby(['loc', 'player'])['score'].mean().unstack().fillna(0)
+location_count = data['loc'].value_counts() / 3
+victories = data[data['place'] == 1].groupby(['loc', 'player']).size().unstack(fill_value=0)
+
+
 # Create a hover text column
-hover_text = mean_scores.apply(lambda row: '\n'.join([f"{player}: {score:.2f}" for player, score in row.items()]), axis=1)
-hover_text = hover_text.rename('mean_scores')
+hover_text_scores = mean_scores.apply(lambda row: '<br>'.join([f"{player}: {score:.2f}" for player, score in row.items()]), axis=1)
+hover_text_victories = victories.apply(lambda row: '<br>'.join([f"{player} Wins: {wins}" for player, wins in row.items()]), axis=1)
+hover_text = '<br><br>' + "<b>Punktedurchschnitt:</b> " + "<br>" + hover_text_scores + '<br><br>' + '<b>Anzahl Spiele:</b> ' + location_count.reindex(hover_text_scores.index).astype(int).apply(lambda x: f"{x}") + "<br><br>" + hover_text_victories
 # Merge this with your original data
-data = data.merge(hover_text, on='loc', how='left')
+data = data.merge(hover_text.rename('Details'), on='loc', how='left')
 # Ensure unique entries for locations to avoid plot duplication
 data_unique = data.drop_duplicates(subset=['loc'])
 
@@ -31,9 +36,9 @@ fig = px.scatter_geo(data_unique,
                      lat='latitude',
                      lon='longitude',
                      hover_name='loc', 
-                     hover_data={'mean_scores': True, 'latitude': False, 'longitude': False}  # Include mean scores in hoverinfo
+                     hover_data={'Details': True, 'latitude': False, 'longitude': False}  # Include mean scores in hoverinfo
                      )
 
 # Integrate with Streamlit
-st.title('Interactive Map Dashboard')
+st.title('Bwanastan (ab 2022)')
 st.plotly_chart(fig)
