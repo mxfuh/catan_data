13fdeda968072d7009c3b7d3c06e5a6a6ba681c4
Max <max.fuhlendorf@web.de>
2025-03-06 16:49:26 +0100

    code cleanup

--- app.py
+++ app.py
@@ -5,42 +5,33 @@ import streamlit.components.v1 as components
 import plotly.express as px
 import plotly.graph_objects as go
 
+from catan_functions import fit_excel_into_df
+from catan_functions import create_hover_data
 
-# stadia maps api key
+
+
+# stadia maps api key mit url
 stadia_api_key = "de89fcde-b15e-4db3-a3d6-08e486eb9af6"
-# Construct the style URL for Stadia Maps' "stamen-watercolor" style.
-#style_url = f"https://basemaps-api.stadiamaps.com/v1/styles/stamen-watercolor/style.json?api_key={stadia_api_key}"
-style_url = "https://tiles.stadiamaps.com/styles/stamen_watercolor.json"
 api_style_url = "https://tiles.stadiamaps.com/styles/stamen_watercolor.json?api_key=de89fcde-b15e-4db3-a3d6-08e486eb9af6"
 
+##############################################                 
+########## get data from excel file ##########
+##############################################
 
-cols = list(pd.read_excel("catan_data.xlsx").iloc[0,:])
-data = pd.read_excel("catan_data.xlsx").iloc[1:,:]
-data.columns = cols
+data = fit_excel_into_df("catan_data.xlsx")
 
-
-# Split 'geoloc' into two columns 'latitude' and 'longitude'
-data[['latitude', 'longitude']] = data['geoloc'].str.split(', ', expand=True)
-data['latitude'] = pd.to_numeric(data['latitude'])
-data['longitude'] = pd.to_numeric(data['longitude'])
+##############################################
+##############################################
 
 
-# Calculate mean scores for each player at each location
-mean_scores = data.groupby(['loc', 'player'])['score'].mean().unstack().fillna(0)
-location_count = data['loc'].value_counts() / 3
-victories = data[data['place'] == 1].groupby(['loc', 'player']).size().unstack(fill_value=0)
 
 
-# Create a hover text column
-hover_text_scores = mean_scores.apply(lambda row: '<br>'.join([f"{player}: {score:.2f}" for player, score in row.items()]), axis=1)
-hover_text_victories = victories.apply(lambda row: '<br>'.join([f"{player} Wins: {wins}" for player, wins in row.items()]), axis=1)
-hover_text = '<br><br>' + "<b>Punktedurchschnitt:</b> " + "<br>" + hover_text_scores + '<br><br>' + '<b>Anzahl Spiele:</b> ' + location_count.reindex(hover_text_scores.index).astype(int).apply(lambda x: f"{x}") + "<br><br>" + hover_text_victories
-# Merge this with your original data
-data = data.merge(hover_text.rename('Details'), on='loc', how='left')
-# Ensure unique entries for locations to avoid plot duplication
-data_unique = data.drop_duplicates(subset=['loc'])
+##############################################################
+############# CREATE INTERACTIVE AND COOL MAP ################
+##############################################################
 
-
+# create hover data
+data_unique = create_hover_data(data)
 
 # Use the custom CSS class in your title
 st.markdown(
@@ -52,9 +43,7 @@ st.markdown(
     unsafe_allow_html=True
 )
 
-
-
-# Your map code (using scatter_mapbox as an example)
+# map code (using scatter_mapbox as an example)
 fig = px.scatter_mapbox(
     data_unique,
     lat='latitude',
@@ -76,14 +65,6 @@ fig.update_layout(
     height=400,
     margin=dict(l=2, r=2, t=2, b=2)
 )
-
-
-# # Wrap the Plotly chart in a div with the Victorian frame
-# st.markdown('<div class="victorian-frame">', unsafe_allow_html=True)
-# st.title('Bwanastan (ab 2022)')
-# st.plotly_chart(fig, use_container_width=True)
-# st.markdown('</div>', unsafe_allow_html=True)
-
 
 # Create an HTML snippet with updated CSS for responsiveness and centering.
 html_string = f"""
@@ -124,6 +105,11 @@ html_string = f"""
 
 # Embed the HTML with the custom frame in your Streamlit app
 components.html(html_string,height=510, width=630 ) # 
+
+
+
+#####################################################
+#####################################################
 
 
 
